7a8e117b104d2daf6e9242837db9ec27da09a06d
Bishwojitdas <bishwoji88@gmail.com>
2022-12-19 21:09:46 +0600

    Chivo font and color added

--- lib/screen/utils/customiazationFont.dart
+++ lib/screen/utils/customiazationFont.dart
@@ -30,3 +30,57 @@ const LatoThin=TextStyle(
   fontSize: Dimensions.FontSizeDefault,
   fontWeight: FontWeight.w100,
 );
+
+const ChivoMonoBlack=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w900,
+);
+
+const ChivoMonoExtraBold=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w800,
+);
+
+const ChivoMonoBold=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w700,
+);
+
+const ChivoMonoSemiBold=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w600,
+);
+
+const ChivoMonoMedium=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w500,
+);
+
+const ChivoMonoRegular=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w400,
+);
+
+const ChivoMonoLight=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w300,
+);
+
+const ChivoMonoExtraLight=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w200,
+);
+
+const ChivoMonoThin=TextStyle(
+  fontFamily: 'Chivo Mono',
+  fontSize: Dimensions.FontSizeDefault,
+  fontWeight: FontWeight.w100,
+);
